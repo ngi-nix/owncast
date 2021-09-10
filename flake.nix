1b60054fdc58c1cefb98884410c4b2a2101fb8d1
Mihai Fufezan <fufexan@pm.me>
2021-09-10 15:24:14 +0300

    flake.nix: replace implementation with better one from nixpkgs PR

--- flake.nix
+++ flake.nix
@@ -18,45 +18,34 @@
     in
     {
       overlay = final: prev: with prev; {
-        owncast =
-          let
-            setupScript = ''
-              [ ! -d $(pwd)/static ] && (
-                cp -r ${placeholder "out"}/share/owncast/{static,webroot} $(pwd)
-                chmod -R u+w $(pwd)/{static,webroot}
-              )
+        owncast = buildGoModule {
+          pname = "owncast";
+          version = "dirty"; #self.shortRev or "${nixpkgs.lib.substring 0 8 self.lastModifiedDate}-dev"; # "x.x.x" for releases
+
+          propagatedBuildInputs = [ ffmpeg ];
+
+          nativeBuildInputs = [ makeWrapper ];
+
+          src = ./.;
+
+          vendorSha256 = "sha256-jx2dJbG8ebjGkyE5D3jUHkmw/nfjeqM38iwmO+7i6oA=";
+
+          postInstall = ''
+            wrapProgram $out/bin/owncast --prefix PATH : ${
+              lib.makeBinPath [ bash ffmpeg which ]
+            }
+          '';
+
+          meta = {
+            homepage = "https://owncast.online";
+            description = ''
+              Owncast is a self-hosted live video and web chat server for use
+              with existing popular broadcasting software
             '';
-          in
-          buildGoModule {
-            pname = "owncast";
-            version = "dirty"; #self.shortRev or "${nixpkgs.lib.substring 0 8 self.lastModifiedDate}-dev"; # "x.x.x" for releases
-
-            nativeBuildInputs = [ makeWrapper ];
-
-            src = ./.;
-
-            vendorSha256 = "sha256-jx2dJbG8ebjGkyE5D3jUHkmw/nfjeqM38iwmO+7i6oA=";
-
-            preInstall = ''
-              mkdir -p $out/share/owncast
-              cp -r $src/{static,webroot} $out/share/owncast
-            '';
-
-            postInstall = ''
-              wrapProgram $out/bin/owncast \
-                --run '${setupScript}' \
-                --prefix PATH : ${lib.makeBinPath [ bash ffmpeg which ]}
-            '';
-
-            meta = {
-              homepage = "https://owncast.online";
-              description = ''
-                Owncast is a self-hosted live video and web chat server for use
-                with existing popular broadcasting software
-              '';
-              license = lib.licenses.mit;
-            };
+            license = lib.licenses.mit;
+            platforms = lib.platforms.unix;
           };
+        };
       };
 
       packages = forAllSystems (system: {
@@ -69,123 +58,107 @@
       devShell = self.defaultPackage;
 
       nixosModules.owncast =
-        with nixpkgs.lib;
-        { config, pkgs, ... }:
-        let
-          cfg = config.services.owncast;
+        { lib, pkgs, config, ... }:
+        let cfg = config.services.owncast;
         in
+        with lib;
         {
-          options = {
-            services.owncast = {
-              enable = mkEnableOption "Jellyfin Media Server";
 
-              package = mkOption {
-                type = types.package;
-                default = pkgs.owncast;
-                example = literalExample "pkgs.owncast";
-                description = ''
-                  Owncast package to use.
-                '';
-              };
+          options.services.owncast = {
 
-              openFirewall = mkOption {
-                type = types.bool;
-                default = false;
-                description = ''
-                  Open the default ports in the firewall for the server. The
-                  HTTP/HTTPS ports can be changed in the Web UI, so this option should
-                  only be used if they are unchanged.
-                '';
-              };
+            enable = mkEnableOption "owncast";
 
-              httpPort = mkOption {
-                type = types.int;
-                default = 8080;
-                description = ''
-                  HTTP Port to use.
-                '';
-              };
-
-              rtmpPort = mkOption {
-                type = types.int;
-                default = 1935;
-                description = ''
-                  RTMP Port to use.
-                '';
-              };
-
-              streamkey = mkOption {
-                type = types.str;
-                default = "abc123";
-                description = ''
-                  Stream key to use.
-                '';
-              };
+            dataDir = mkOption {
+              type = types.str;
+              default = "/var/lib/owncast";
+              description = "The directory where owncast stores its data files.";
             };
+
+            openFirewall = mkOption {
+              type = types.bool;
+              default = false;
+              description = ''
+                Open ports in the firewall for owncast.
+              '';
+            };
+
+            user = mkOption {
+              type = types.str;
+              default = "owncast";
+              description = "User account under which owncast runs.";
+            };
+
+            group = mkOption {
+              type = types.str;
+              default = "owncast";
+              description = "Group under which owncast runs.";
+            };
+
+            listen = mkOption {
+              type = types.str;
+              default = "0.0.0.0";
+              example = "127.0.0.1";
+              description = "The IP address to bind owncast to.";
+            };
+
+            port = mkOption {
+              type = types.port;
+              default = 80;
+              description = ''
+                TCP port where owncast web-gui listens.
+              '';
+            };
+
+            rtmp-port = mkOption {
+              type = types.port;
+              default = 1935;
+              description = ''
+                TCP port where owncast rtmp service listens.
+              '';
+            };
+
           };
 
           config = mkIf cfg.enable {
+
+            systemd.tmpfiles.rules = [
+              "L+ '${cfg.dataDir}/static' - - - - ${pkgs.owncast.src}/static"
+              "C '${cfg.dataDir}/webroot' 0700 - - - ${pkgs.owncast.src}/webroot"
+            ];
+
             systemd.services.owncast = {
-              description = "Owncast Server";
-              after = [ "network.target" ];
-              wantedBy = [ "multi-user.target" ];
+              wantedBy = [ "default.target" ];
 
-              serviceConfig = rec {
-                User = "owncast";
-                Group = "owncast";
-                WorkingDirectory = "/var/lib/owncast";
-                #StateDirectory = "owncast";
-                #CacheDirectory = "${StateDirectory}/logs";
-                ExecStart = ''
-                  ${cfg.package}/bin/owncast \
-                  -webserverport ${toString cfg.httpPort} \
-                  -rtmpport ${toString cfg.rtmpPort} \
-                  -streamkey ${cfg.streamkey} \
-                '';
-                /*    -logdir /var/lib/${CacheDirectory} \
-                  -database /var/lib/${StateDirectory}/owncast.db
-                  '';
-                */
+              serviceConfig = {
+                User = cfg.user;
+                Group = cfg.group;
+                WorkingDirectory = cfg.dataDir;
+                StateDirectory = baseNameOf cfg.dataDir;
+                ExecStart = "${pkgs.owncast}/bin/owncast -webserverport ${toString cfg.port} -rtmpport ${toString cfg.rtmp-port} -webserverip ${cfg.listen}";
                 Restart = "on-failure";
+              };
 
-                # Security options:
-
-                NoNewPrivileges = true;
-
-                AmbientCapabilities = "";
-                CapabilityBoundingSet = "";
-
-                # ProtectClock= adds DeviceAllow=char-rtc r
-                DeviceAllow = "";
-
-                LockPersonality = true;
-
-                PrivateTmp = true;
-                PrivateDevices = true;
-                PrivateUsers = true;
-
-                ProtectClock = true;
-                ProtectControlGroups = true;
-                ProtectHostname = true;
-                ProtectKernelLogs = true;
-                ProtectKernelModules = true;
-                ProtectKernelTunables = true;
-
-                RemoveIPC = true;
-
-                RestrictNamespaces = true;
+              environment = {
+                LC_ALL = "en_US.UTF-8";
+                LANG = "en_US.UTF-8";
               };
             };
 
-            networking.firewall = mkIf cfg.openFirewall {
-              allowedTCPPorts = with cfg; [ rtmpPort httpPort ];
+            users.users = mkIf (cfg.user == "owncast") {
+              owncast = {
+                isSystemUser = true;
+                group = cfg.group;
+                description = "owncast system user";
+              };
             };
 
-            users = {
-              users.owncast.isSystemUser = true;
-              groups.owncast = { };
-            };
+            users.groups = mkIf (cfg.group == "owncast") { ${cfg.group} = { }; };
+
+            networking.firewall =
+              mkIf cfg.openFirewall { allowedTCPPorts = [ cfg.port cfg.rtmp-port ]; };
+
           };
+          meta = { maintainers = with lib.maintainers; [ mayniklas ]; };
         };
 
       nixosModule = self.nixosModules.owncast;
@@ -203,7 +176,7 @@
               nodes = {
                 client = { ... }: {
                   imports = [ self.nixosModules.owncast ];
-                  nixpkgs.config.overlays = [ self.overlay ];
+                  nixpkgs.overlays = [ self.overlay ];
                   services.owncast.enable = true;
                 };
               };
