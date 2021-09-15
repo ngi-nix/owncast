6abc17a78dd14b4c4ad490d2decf68e68af5582b
Mihai Fufezan <fufexan@pm.me>
2021-09-15 15:44:54 +0300

    remove module and tests

--- flake.nix
+++ flake.nix
@@ -5,7 +5,13 @@
 
   outputs = { self, nixpkgs }:
     let
-      supportedSystems = [ "aarch64-linux" "i686-linux" "x86_64-linux" ];
+      supportedSystems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
 
       forAllSystems = f:
         nixpkgs.lib.genAttrs supportedSystems (system: f system);
@@ -20,7 +26,7 @@
       overlay = final: prev: with prev; {
         owncast = buildGoModule rec {
           pname = "owncast";
-          version = "dirty";
+          version = self.shortRev or "${nixpkgs.lib.substring 0 8 self.lastModifiedDate}-dev"; # "x.x.x" for releases
 
           src = ./.;
 
@@ -38,19 +44,14 @@
           postInstall =
             let
               setupScript = ''
-                ([ ! -d $PWD/static ] && [ ! -d $PWD/webroot} ]) && (
-                  cp -r ${placeholder "out"}/webroot $PWD/webroot
-                  ln -s ${placeholder "out"}/static $PWD/static
-                  chmod -R u+w $PWD/webroot
-                )
+                [ ! -d $PWD/static ] && ln -s ${placeholder "out"}/static $PWD
+                [ ! -d $PWD/webroot ] && cp --no-preserve=mode -r ${placeholder "out"}/webroot $PWD
               '';
             in
             ''
               wrapProgram $out/bin/owncast \
                 --run '${setupScript}' \
-                --prefix PATH : ${
-                  lib.makeBinPath [ bash which ffmpeg ]
-                }
+                --prefix PATH : ${lib.makeBinPath [ bash which ffmpeg ]}
             '';
 
           installCheckPhase = ''
@@ -64,7 +65,6 @@
             homepage = "https://owncast.online";
             license = licenses.mit;
             platforms = platforms.unix;
-            maintainers = with maintainers; [ mayniklas ];
           };
         };
       };
@@ -77,140 +77,5 @@
         self.packages.${system}.owncast);
 
       devShell = self.defaultPackage;
-
-      nixosModules.owncast =
-        { lib, pkgs, config, ... }:
-          with lib;
-          let cfg = config.services.owncast;
-          in
-          {
-
-            options.services.owncast = {
-
-              enable = mkEnableOption "owncast";
-
-              dataDir = mkOption {
-                type = types.str;
-                default = "/var/lib/owncast";
-                description = ''
-                  The directory where owncast stores its data files.
-                '';
-              };
-
-              openFirewall = mkOption {
-                type = types.bool;
-                default = false;
-                description = ''
-                  Open the appropriate ports in the firewall for owncast.
-                '';
-              };
-
-              user = mkOption {
-                type = types.str;
-                default = "owncast";
-                description = "User account under which owncast runs.";
-              };
-
-              group = mkOption {
-                type = types.str;
-                default = "owncast";
-                description = "Group under which owncast runs.";
-              };
-
-              listen = mkOption {
-                type = types.str;
-                default = "127.0.0.1";
-                example = "0.0.0.0";
-                description = "The IP address to bind the owncast server to.";
-              };
-
-              port = mkOption {
-                type = types.port;
-                default = 80;
-                description = ''
-                  TCP port where owncast web-gui listens.
-                '';
-              };
-
-              rtmp-port = mkOption {
-                type = types.port;
-                default = 1935;
-                description = ''
-                  TCP port where owncast rtmp service listens.
-                '';
-              };
-
-            };
-
-            config = mkIf cfg.enable {
-
-              systemd.services.owncast = {
-                description = "A self-hosted live video and web chat server";
-                wantedBy = [ "default.target" ];
-
-                serviceConfig = {
-                  User = cfg.user;
-                  Group = cfg.group;
-                  WorkingDirectory = cfg.dataDir;
-                  ExecStart = "${pkgs.owncast}/bin/owncast -webserverport ${toString cfg.port} -rtmpport ${toString cfg.rtmp-port} -webserverip ${cfg.listen}";
-                  Restart = "on-failure";
-                  StateDirectory = cfg.dataDir;
-                };
-
-                environment = {
-                  LC_ALL = "en_US.UTF-8";
-                  LANG = "en_US.UTF-8";
-                };
-              };
-
-              users.users = mkIf (cfg.user == "owncast") {
-                owncast = {
-                  isSystemUser = true;
-                  group = cfg.group;
-                  description = "owncast system user";
-                };
-              };
-
-              users.groups = mkIf (cfg.group == "owncast") { owncast = { }; };
-
-              networking.firewall =
-                mkIf cfg.openFirewall { allowedTCPPorts = [ cfg.rtmp-port ] ++ optional (cfg.listen != "127.0.0.1") cfg.port; };
-
-            };
-            meta = { maintainers = with lib.maintainers; [ mayniklas ]; };
-          };
-
-      nixosModule = self.nixosModules.owncast;
-
-      checks = forAllSystems (system:
-        with nixpkgsFor.${system};
-        {
-          inherit (self.packages.${system}) owncast;
-
-          # A VM test of the NixOS module.
-          vmTest =
-            with import (nixpkgs + "/nixos/lib/testing-python.nix") { inherit system; };
-
-            makeTest {
-              nodes = {
-                client = { ... }: {
-                  imports = [ self.nixosModules.owncast ];
-                  environment.systemPackages = [ curl ];
-                  nixpkgs.overlays = [ self.overlay ];
-                  services.owncast = {
-                    enable = true;
-                    port = 8080;
-                  };
-                };
-              };
-
-              testScript =
-                ''
-                  start_all()
-                  client.wait_for_unit("owncast.service")
-                  client.succeed("curl localhost:8080/api/status")
-                '';
-            };
-        });
     };
 }
